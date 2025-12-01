a8db99bc1fef6f7b483d86fc61ef9705aa885d74
Mathias Norrback <mathias.norrbacken@gmail.com>
2025-12-01 15:30:05 +0100

    Add new classes

---
+++ CustomersApp/Customer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomersApp;
+
+internal class Customer
+{
+}
---
+++ CustomersApp/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomersApp;
+
+internal class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public double PriceTotal { get; set; }
+    public Customer Customer { get; set; } = null!;
+}
